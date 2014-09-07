c116a24d29f324f7471b5f811549e4777bea28cd
ashrestha <ashrestha@Zalando-07287.corp.ad.zalando.net>
2014-09-07 14:54:40 +0200

    refactoring

--- src/main/java/de/zalando/mentoring/lunchlottery/controller/UserController.java
+++ src/main/java/de/zalando/mentoring/lunchlottery/controller/UserController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-@RequestMapping("/helloController")
+@RequestMapping("/user")
 @Controller
-public class HelloController {
+public class UserController {
 
-	@RequestMapping(value="/",method = RequestMethod.GET)
+	@RequestMapping(value="/home",method = RequestMethod.GET)
 	public ModelAndView welcomePage() {
 
 		ModelAndView model = new ModelAndView();
